185a55151089bb7494eb70a858d854f736726737
naufalparros14 <naufalparros14@gmail.com>
2025-10-13 13:17:14 +0700

    pull

--- resources/views/admin/superadmin/members/index.blade.php
+++ resources/views/admin/superadmin/members/index.blade.php
@@ -13,7 +13,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
             </svg>
-            Kembali ke Dashboard
+            <- Kembali ke Dashboard
         </a>
     </div>
 
